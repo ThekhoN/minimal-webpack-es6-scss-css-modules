9c3202662404282569d07cd0b059457829265991
Ngaosathe <thekho.ngaosathe@jasperindia.local>
2017-07-04 16:41:55 +0530

    optimized support for css-modules

--- src/js/modules/render-result/index.js
+++ src/js/modules/render-result/index.js
@@ -1,7 +1,7 @@
-import './style.css';
+import styles from './style.css';
 
 const renderResult = (node, content) => {
-  node.innerHTML = `<h2 class={style.main}>Result: <span class='special'>${content}<span></h2>`;
+  node.innerHTML = `<h2 class=${styles.main}>Result: <span class=${styles.special}>${content}<span></h2>`;
 };
 
 export default renderResult;
