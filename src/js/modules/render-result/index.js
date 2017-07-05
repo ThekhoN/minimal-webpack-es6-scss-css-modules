4fd2fbda2de97b4141c650c4d0119909a3cdceea
Ngaosathe <thekho.ngaosathe@jasperindia.local>
2017-07-05 12:27:32 +0530

    added spread operator support

--- src/js/modules/render-result/index.js
+++ src/js/modules/render-result/index.js
@@ -1,5 +1,8 @@
 import styles from './style.css';
 
+const addAllArgs = (...args) => [...args].reduce((a, b) => a + b, 0);
+console.log('result of addAllArgs: ', addAllArgs(1, 2, 3, 4, 5));
+
 const renderResult = (node, content) => {
   node.innerHTML = `<h2 class=${styles.main}>Result: <span class=${styles.special}>${content}<span></h2>`;
 };
